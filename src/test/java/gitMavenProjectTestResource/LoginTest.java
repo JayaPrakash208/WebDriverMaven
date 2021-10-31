c42306895ca355f4a65d058bde8eabc40f59c448
jp_git <jayaprakashnarayaan208@gmail.com>
2021-10-31 21:09:14 +0530

    Adding the Maven Project

---
+++ src/test/java/gitMavenProjectTestResource/LoginTest.java
@@ -0,0 +1,26 @@
+package gitMavenProjectTestResource;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LoginTest {
+	public static WebDriver driver = new ChromeDriver();
+	
+	@BeforeSuite
+	public static void openWebsite() {
+		WebDriverManager.chromedriver().setup();
+		driver.get("https://mail.google.com/");
+	}
+	@Test
+	public static void ClickOnSignin() {
+		WebElement SigninButton = driver.findElement(By.xpath("//a[text()='Sign in']"));
+		SigninButton.click(); 
+	}
+
+}
